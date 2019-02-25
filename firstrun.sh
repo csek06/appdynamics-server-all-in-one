76345aece9091d8fbbfd841bc0b4fd37ce9e155d
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-02-25 14:57:42 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -11,7 +11,7 @@ if [ ! -z $VERSION ]; then
 else
   #Check the latest version on appdynamics
   curl -s -L -o tmpout.json "https://download.appdynamics.com/download/downloadfile/?version=&apm=&os=linux&platform_admin_os=linux&events=&eum="
-VERSION=$(grep -oP '(\"version\"\:\")\K(.*?)(?=\"\,\")' tmpout.json)
+  VERSION=$(grep -oP '(\"version\"\:\")\K(.*?)(?=\"\,\")' tmpout.json)
   DOWNLOAD_PATH=$(grep -oP '(\"download_path\"\:\")\K(.*?)(?=\"\,\")' tmpout.json)
   VERSION=${VERSION:1}
   echo "Latest version on appdynamics is" $VERSION
@@ -19,6 +19,8 @@ fi
 
 if [ ! -f /config/appdynamics-"$VERSION"/ ]; then
   echo "Installing version '$VERSION'"
+  TOKEN=$(curl -X POST -d '{"username": "$appd-user","password": "appd-pass","scopes": ["download"]}' https://identity.msrv.saas.appdynamics.com/v2.0/oauth/token | grep -oP '(\"access_token\"\:\s\")\K(.*?)(?=\"\,\s\")')
+  curl -L -O -H "Authorization: Bearer ${TOKEN}" https://download.appdynamics.com/download/prox/download-file/enterprise-console/4.5.6.17299/platform-setup-x64-linux-4.5.6.17299.sh
   wget ${DOWNLOAD_PATH}
   echo "file downloaded"
 else
