bfc4f197fa4e71ecba2ace1b3c3592161d07ff95
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-04 15:53:31 -0500

    update startup

--- startup-scripts/start-EC.sh
+++ startup-scripts/start-EC.sh
@@ -2,7 +2,7 @@ PA_FILE=/config/appdynamics/platform/platform-admin/bin/platform-admin.sh
 if [ -f "$PA_FILE" ]; then
 	echo "Starting Enterprise Console"
 	cd /config/appdynamics/platform/platform-admin/bin/
-	sh $PA_FILE start-platform-admin
+	sh ./platform-admin.sh start-platform-admin
 else
 	echo "Platform Admin not found here - $PA_FILE"
 fi
--- startup-scripts/start-ES.sh
+++ startup-scripts/start-ES.sh
@@ -2,7 +2,7 @@ PA_FILE=/config/appdynamics/platform/platform-admin/bin/platform-admin.sh
 if [ -f "$PA_FILE" ]; then
 	echo "Starting Events Service"
 	cd /config/appdynamics/platform/platform-admin/bin/
-	sh $PA_FILE start-events-service
+	sh ./platform-admin.sh start-events-service
 else
 	echo "Platform Admin not found here - $PA_FILE"
 fi
