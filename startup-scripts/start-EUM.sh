39ac4e3c421ff68efc31998f31dc74152dd6f498
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-04 16:31:52 -0500

    Update start-EUM.sh

--- startup-scripts/start-EUM.sh
+++ startup-scripts/start-EUM.sh
@@ -1,7 +1,8 @@
 EUM_FILE=/config/appdynamics/EUM/eum-processor/bin/eum.sh
 if [ -f "$EUM_FILE" ]; then
 	echo "Starting EUM Server"
-	sh $EUM_FILE start-events-service
+	cd /config/appdynamics/EUM/eum-processor/
+	./bin/eum.sh start
 else
 	echo "EUM File not found here - $EUM_FILE"
 fi
