d6fb545117f39b1bfd05937f02510bfa0645621c
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-04 15:46:04 -0500

    Updated Startup
    needed to move to platformadmin/bin directory for pid file creation.

--- startup-scripts/start-Controller.sh
+++ startup-scripts/start-Controller.sh
@@ -1,6 +1,7 @@
 PA_FILE=/config/appdynamics/platform/platform-admin/bin/platform-admin.sh
 if [ -f "$PA_FILE" ]; then
 	echo "Starting Controller with DB"
+	cd /config/appdynamics/platform/platform-admin/bin/
 	sh $PA_FILE start-controller-appserver --with-db
 else
 	echo "Platform Admin not found here - $PA_FILE"
--- startup-scripts/start-EC.sh
+++ startup-scripts/start-EC.sh
@@ -1,6 +1,7 @@
 PA_FILE=/config/appdynamics/platform/platform-admin/bin/platform-admin.sh
 if [ -f "$PA_FILE" ]; then
 	echo "Starting Enterprise Console"
+	cd /config/appdynamics/platform/platform-admin/bin/
 	sh $PA_FILE start-platform-admin
 else
 	echo "Platform Admin not found here - $PA_FILE"
