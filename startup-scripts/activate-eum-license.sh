101d5438d5c6062113f75024ba9c911f6ca41779
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-05 12:00:14 -0500

    fixing indents

--- startup-scripts/activate-eum-license.sh
+++ startup-scripts/activate-eum-license.sh
@@ -6,8 +6,8 @@ LICENSE_OG="/config/license.lic"
 LICENSE_LOC="/config/appdynamics/controller/controller/license.lic"
 cd /config/appdynamics/EUM/eum-processor/
 if [ -f $LICENSE_OG ]; then
-  ./bin/provision-license $LICENSE_OG
+	./bin/provision-license $LICENSE_OG
 fi
 if [ -f $LICENSE_LOC ]; then
-  ./bin/provision-license $LICENSE_LOC
+	./bin/provision-license $LICENSE_LOC
 fi
