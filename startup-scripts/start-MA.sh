6c61e5340323db4f68fb01578b039dc0d6f77545
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-07 19:10:46 -0500

    Update start-MA.sh

--- startup-scripts/start-MA.sh
+++ startup-scripts/start-MA.sh
@@ -18,10 +18,10 @@ if [ -z $ENABLE_SIM ]; then
 	ENABLE_SIM="false"
 fi
 if [ -z $ENABLE_SIM_DOCKER ]; then
-	ENABLE_SIM_DOCKER="true"
+	ENABLE_SIM_DOCKER="false"
 fi
 if [ -z $ENABLE_CONTAINERIDASHOSTID ]; then
-	ENABLE_CONTAINERIDASHOSTID="true"
+	ENABLE_CONTAINERIDASHOSTID="false"
 fi
 
 MA_FILE=/config/appdynamics/machine-agent/bin/machine-agent
