55d5868c3fda0ba10831f5216edfd6347c622783
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-11 11:23:13 -0400

    Update start-ES.sh

--- startup-scripts/start-ES.sh
+++ startup-scripts/start-ES.sh
@@ -4,6 +4,7 @@ PA_FILE=/config/appdynamics/platform/platform-admin/bin/platform-admin.sh
 if [ -f "$PA_FILE" ]; then
 	echo "Starting Events Service"
 	cd /config/appdynamics/platform/platform-admin/bin/
+	./platform-admin.sh login --user-name admin --password appd
 	./platform-admin.sh start-events-service
 else
 	echo "Platform Admin not found here - $PA_FILE"
