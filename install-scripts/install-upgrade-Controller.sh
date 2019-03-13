a9f3de139748f12961314a51c708efbd2495366e
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-13 14:53:40 -0400

    Update install-upgrade-Controller.sh

--- install-scripts/install-upgrade-Controller.sh
+++ install-scripts/install-upgrade-Controller.sh
@@ -2,7 +2,7 @@
 
 # check if Controller is installed
 if [ -f /config/appdynamics/controller/controller/bin/controller.sh ]; then
-	cd /appdynamics/controller/controller/appserver/glassfish/domains/domain1/applications/controller/META-INF
+	cd /config/appdynamics/controller/controller/appserver/glassfish/domains/domain1/applications/controller/META-INF
 	INSTALLED_VERSION=$(grep -oPz '(Controller\s*?v)\K(.*?)(?=\s.*?Build)' * | tr -d '\0')
 	echo "Controller: $INSTALLED_VERSION is installed"
 	# check for upgrade <code to be inserted>, however upgrade path needs to be followed EC > ES > EUM > Controller
