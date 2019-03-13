80053d032b5532d6ef46b64ecfe34cdfea51b867
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-13 15:09:49 -0400

    update install locations

--- startup-scripts/start-Controller.sh
+++ startup-scripts/start-Controller.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PA_FILE=/config/appdynamics/platform/platform-admin/bin/platform-admin.sh
+PA_FILE=/config/appdynamics/enterprise-console/platform-admin/bin/platform-admin.sh
 if [ -f "$PA_FILE" ]; then
 	echo "Starting Controller with DB"
-	cd /config/appdynamics/platform/platform-admin/bin/
+	cd /config/appdynamics/enterprise-console/platform-admin/bin/
 	./platform-admin.sh login --user-name admin --password appd
 	./platform-admin.sh start-controller-appserver --with-db
 else
