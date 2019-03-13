c70a0b9ee56157b2b56e0a0fc96d54ed33143490
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-13 13:26:02 -0400

    Update install-upgrade-ES.sh
    modified if ES is only SCENARIO variable

--- install-scripts/install-upgrade-ES.sh
+++ install-scripts/install-upgrade-ES.sh
@@ -7,8 +7,18 @@ if [ -f /config/appdynamics/controller/events-service/processor/bin/events-servi
 	echo "Events Service: $INSTALLED_VERSION is installed"
 	# check for upgrade <code to be inserted>, however upgrade path needs to be followed EC > ES > EUM > Controller
 else
+	# check if enterprise console is installed
+	if [ ! -f /config/appdynamics/platform/platform-admin/bin/platform-admin.sh ]; then
+		EC_INSTALL_UPGRADE_FILE=/your-platform-install/install-scripts/install-upgrade-EC.sh
+		if [ -f "$EC_INSTALL_UPGRADE_FILE" ]; then
+			chmod +x $EC_INSTALL_UPGRADE_FILE
+			bash $EC_INSTALL_UPGRADE_FILE
+		else
+			echo "EC install file not found here - $EC_INSTALL_UPGRADE_FILE"
+		fi
+	fi
 	echo "Installing Events Service"
 	cd /config/appdynamics/platform/platform-admin/bin
-	./platform-admin.sh install-events-service  --profile dev --hosts localhost
+	./platform-admin.sh install-events-service  --profile dev --hosts $SERVERIP
 fi
 
