8b2c7e99822d15702e694c4beaac636df7f0a128
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-13 15:28:10 -0400

    Update install-upgrade-ES.sh

--- install-scripts/install-upgrade-ES.sh
+++ install-scripts/install-upgrade-ES.sh
@@ -9,16 +9,11 @@ if [ -f /config/appdynamics/events-service/processor/bin/events-service.sh ]; th
 else
 	# check if enterprise console is installed
 	if [ ! -f /config/appdynamics/enterprise-console/platform-admin/bin/platform-admin.sh ]; then
-		EC_INSTALL_UPGRADE_FILE=/your-platform-install/install-scripts/install-upgrade-EC.sh
-		if [ -f "$EC_INSTALL_UPGRADE_FILE" ]; then
-			chmod +x $EC_INSTALL_UPGRADE_FILE
-			bash $EC_INSTALL_UPGRADE_FILE
-		else
-			echo "EC install file not found here - $EC_INSTALL_UPGRADE_FILE"
-		fi
+		echo "Please install Enterprise Console on Host and map appdata to /config"
+	else
+		echo "Installing Events Service"
+		cd /config/appdynamics/enterprise-console/platform-admin/bin
+		./platform-admin.sh install-events-service  --profile dev --hosts $SERVERIP
 	fi
-	echo "Installing Events Service"
-	cd /config/appdynamics/enterprise-console/platform-admin/bin
-	./platform-admin.sh install-events-service  --profile dev --hosts $SERVERIP
 fi
 
