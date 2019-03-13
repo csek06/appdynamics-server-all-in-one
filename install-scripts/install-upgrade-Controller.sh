d8bb72644ac14b3c00a4740df9f19046a9d47ab8
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-13 15:04:16 -0400

    Update install-upgrade-Controller.sh

--- install-scripts/install-upgrade-Controller.sh
+++ install-scripts/install-upgrade-Controller.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # check if Controller is installed
-if [ -f /config/appdynamics/controller/controller/bin/controller.sh ]; then
-	cd /config/appdynamics/controller/controller/appserver/glassfish/domains/domain1/applications/controller/META-INF
+if [ -f /config/appdynamics/controller/bin/controller.sh ]; then
+	cd /config/appdynamics/controller/appserver/glassfish/domains/domain1/applications/controller/META-INF
 	INSTALLED_VERSION=$(grep -oPz '(Controller\s*?v)\K(.*?)(?=\s.*?Build)' MANIFEST.MF)
 	echo "Controller: $INSTALLED_VERSION is installed"
 	# check for upgrade <code to be inserted>, however upgrade path needs to be followed EC > ES > EUM > Controller
 else
 	# check if enterprise console is installed
-	if [ ! -f /config/appdynamics/platform/platform-admin/bin/platform-admin.sh ]; then
+	if [ ! -f /config/appdynamics/enterprise-console/platform-admin/bin/platform-admin.sh ]; then
 		EC_INSTALL_UPGRADE_FILE=/your-platform-install/install-scripts/install-upgrade-EC.sh
 		if [ -f "$EC_INSTALL_UPGRADE_FILE" ]; then
 			chmod +x $EC_INSTALL_UPGRADE_FILE
@@ -18,7 +18,7 @@ else
 		fi
 	fi
 	echo "Installing Controller and local database"
-	cd /config/appdynamics/platform/platform-admin/bin
+	cd /config/appdynamics/enterprise-console/platform-admin/bin
 	./platform-admin.sh create-platform --name my-platform --installation-dir /config/appdynamics/
 	./platform-admin.sh add-hosts --hosts $SERVERIP
 	./platform-admin.sh submit-job --service controller --job install --args controllerPrimaryHost=$SERVERIP controllerAdminUsername=admin controllerAdminPassword=appd controllerRootUserPassword=appd mysqlRootPassword=appd
