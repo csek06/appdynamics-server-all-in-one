ca0ac5d2b75c6b77a36949ec89807431edf60fdb
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-03-13 15:05:04 -0400

    Update install-upgrade-ES.sh

--- install-scripts/install-upgrade-ES.sh
+++ install-scripts/install-upgrade-ES.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # check if Events Service is installed
-if [ -f /config/appdynamics/controller/events-service/processor/bin/events-service.sh ]; then
-	cd /config/appdynamics/controller/events-service/processor
+if [ -f /config/appdynamics/events-service/processor/bin/events-service.sh ]; then
+	cd /config/appdynamics/events-service/processor
 	INSTALLED_VERSION=$(grep -oPz '(version=)\K(.*?$)' version.txt)
 	echo "Events Service: $INSTALLED_VERSION is installed"
 	# check for upgrade <code to be inserted>, however upgrade path needs to be followed EC > ES > EUM > Controller
 else
 	# check if enterprise console is installed
-	if [ ! -f /config/appdynamics/platform/platform-admin/bin/platform-admin.sh ]; then
+	if [ ! -f /config/appdynamics/enterprise-console/platform-admin/bin/platform-admin.sh ]; then
 		EC_INSTALL_UPGRADE_FILE=/your-platform-install/install-scripts/install-upgrade-EC.sh
 		if [ -f "$EC_INSTALL_UPGRADE_FILE" ]; then
 			chmod +x $EC_INSTALL_UPGRADE_FILE
@@ -18,7 +18,7 @@ else
 		fi
 	fi
 	echo "Installing Events Service"
-	cd /config/appdynamics/platform/platform-admin/bin
+	cd /config/appdynamics/enterprise-console/platform-admin/bin
 	./platform-admin.sh install-events-service  --profile dev --hosts $SERVERIP
 fi
 
