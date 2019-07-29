0d2175a3e40e1032d74cffdc7c382a276ed799a6
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-07-29 16:17:06 -0400

    updated to use event service host in post config

--- install-scripts/post-install-EUM-Config.sh
+++ install-scripts/post-install-EUM-Config.sh
@@ -25,7 +25,7 @@ sed -i s/onprem.useEncryptedCredentials=.*/onprem.useEncryptedCredentials=false/
 curl -s -c cookie.appd --user root@system:appd -X GET http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/auth?action=login
 X_CSRF_TOKEN="$(grep X-CSRF-TOKEN cookie.appd | grep -oP '(X-CSRF-TOKEN\s)\K(.*)?(?=$)')"
 X_CSRF_TOKEN_HEADER="`if [ -n "$X_CSRF_TOKEN" ]; then echo "X-CSRF-TOKEN:$X_CSRF_TOKEN"; else echo ''; fi`"
-curl -i -v -s -b cookie.appd -c cookie.appd2 -H "$X_CSRF_TOKEN_HEADER" -X POST "http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/rest/configuration?name=eum.es.host&value=http://$EUM_HOST:9080"
+curl -i -v -s -b cookie.appd -c cookie.appd2 -H "$X_CSRF_TOKEN_HEADER" -X POST "http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/rest/configuration?name=eum.es.host&value=http://$EVENT_SERVICE_HOST:9080"
 curl -i -v -s -b cookie.appd -c cookie.appd2 -H "$X_CSRF_TOKEN_HEADER" -X POST  "http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/rest/configuration?name=eum.cloud.host&value=http://$EUM_HOST:7001"
 curl -i -v -s -b cookie.appd -c cookie.appd2 -H "$X_CSRF_TOKEN_HEADER" -X POST  "http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/rest/configuration?name=eum.beacon.host&value=http://$EUM_HOST:7001"
 curl -i -v -s -b cookie.appd -c cookie.appd2 -H "$X_CSRF_TOKEN_HEADER" -X POST  "http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/rest/configuration?name=eum.beacon.https.host&value=https://$EUM_HOST:7002"
