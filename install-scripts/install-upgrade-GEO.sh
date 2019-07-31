548b289042ab0062297396c06d6dfb7204640052
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-07-30 21:05:17 -0400

    Create install-upgrade-GEO.sh

---
+++ install-scripts/install-upgrade-GEO.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# initialize variables
+TOMCAT_FILE=/config/tomcat/bin/startup.sh
+cd /config
+
+if [ -f "$TOMCAT_FILE" ]; then
+	TOMCAT_INSTALL_UPGRADE_FILE=/your-platform-install/install-scripts/install-upgrade-tomcat.sh
+	if [ -f "$TOMCAT_INSTALL_UPGRADE_FILE" ]; then
+		chmod +x $TOMCAT_INSTALL_UPGRADE_FILE
+		bash $TOMCAT_INSTALL_UPGRADE_FILE
+	else
+		echo "Tomcat install file not found here - $TOMCAT_INSTALL_UPGRADE_FILE"
+	fi
+else
+	echo "Tomcat dir not found: ${TOMCAT_HOME}"
+fi 
