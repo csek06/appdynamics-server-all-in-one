af46aa20b1b76d8d6e53e6e7c650703c88282aa1
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-07-31 10:56:44 -0400

    Update install-upgrade-tomcat.sh

--- install-scripts/install-upgrade-tomcat.sh
+++ install-scripts/install-upgrade-tomcat.sh
@@ -5,12 +5,14 @@ TOMCAT_HOME=/config/tomcat
 mkdir -p $TOMCAT_HOME
 cd $TOMCAT_HOME
 
-export JAVA_HOME=/usr/lib/jvm/zulu-12-amd64
-export CATALINA_HOME=$TOMCAT_HOME
-export CATALINA_PID=$TOMCAT_HOME/temp/tomcat.pid
-export CATALINA_BASE=$TOMCAT_HOME
 wget http://apache.mirrors.tds.net/tomcat/tomcat-9/v9.0.22/bin/apache-tomcat-9.0.22.tar.gz
 tar xzvf apache-tomcat-9.0.22.tar.gz -C $TOMCAT_HOME --strip-components=1
+rm apache-tomcat-9.0.22.tar.gz
+
+grep -q JAVA_HOME /etc/environment || echo 'JAVA_HOME="/usr/lib/jvm/zulu-12-amd64"' >> /etc/environment
+grep -q CATALINA_HOME /etc/environment || echo 'CATALINA_HOME="${TOMCAT_HOME}"' >> /etc/environment
+grep -q CATALINA_PID /etc/environment || echo 'CATALINA_PID="${TOMCAT_HOME}/temp/tomcat.pid"' >> /etc/environment
+grep -q CATALINA_BASE /etc/environment || echo 'CATALINA_BASE="${TOMCAT_HOME}"' >> /etc/environment
 
 useradd -s /bin/false -d $TOMCAT_HOME tomcat
 chgrp -R tomcat $TOMCAT_HOME
