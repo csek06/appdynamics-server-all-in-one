fa0297fc770e7db9e546229c94b1e4ed601122ed
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-10-14 15:55:11 -0400

    Update start-DA.sh

--- startup-scripts/start-DA.sh
+++ startup-scripts/start-DA.sh
@@ -11,10 +11,14 @@ fi
 if [ -z $CONTROLLER_KEY ]; then
 	# Connect to Controller and obtain the AccessKey
 	curl -s -c cookie.appd --user admin@customer1:appd -X GET http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/auth?action=login
-	X_CSRF_TOKEN="$(grep X-CSRF-TOKEN cookie.appd | grep -oP '(X-CSRF-TOKEN\s)\K(.*)?(?=$)')"
-	JSESSIONID_H=$(grep -oP '(JSESSIONID\s)\K(.*)?(?=$)' cookie.appd)
-	CONTROLLER_KEY=$(curl http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/restui/user/account -H "X-CSRF-TOKEN: $X_CSRF_TOKEN" -H "Cookie: JSESSIONID=$JSESSIONID_H" | grep -oP '(?:accessKey\"\s\:\s\")\K(.*?)(?=\"\,)')
-	rm cookie.appd
+	if [ -f "cookie.appd" ]; then
+		X_CSRF_TOKEN="$(grep X-CSRF-TOKEN cookie.appd | grep -oP '(X-CSRF-TOKEN\s)\K(.*)?(?=$)')"
+		JSESSIONID_H=$(grep -oP '(JSESSIONID\s)\K(.*)?(?=$)' cookie.appd)
+		CONTROLLER_KEY=$(curl http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/restui/user/account -H "X-CSRF-TOKEN: $X_CSRF_TOKEN" -H "Cookie: JSESSIONID=$JSESSIONID_H" | grep -oP '(?:accessKey\"\s\:\s\")\K(.*?)(?=\"\,)')
+		rm cookie.appd
+	else
+		echo "Couldn't connect to controller to obtain controller key"
+	fi
 fi
 if [ -z $DB_AGENT_NAME ]; then
 	DB_AGENT_NAME=$(hostname)
