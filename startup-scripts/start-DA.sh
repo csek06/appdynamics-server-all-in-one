5970d2f2583522d367218eab8c6715a03cabc986
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-10-17 08:53:21 -0400

    Update start-DA.sh

--- startup-scripts/start-DA.sh
+++ startup-scripts/start-DA.sh
@@ -14,36 +14,38 @@ if [ -z $CONTROLLER_KEY ]; then
 	if [ -f "cookie.appd" ]; then
 		X_CSRF_TOKEN="$(grep X-CSRF-TOKEN cookie.appd | grep -oP '(X-CSRF-TOKEN\s)\K(.*)?(?=$)')"
 		JSESSIONID_H=$(grep -oP '(JSESSIONID\s)\K(.*)?(?=$)' cookie.appd)
-		CONTROLLER_KEY=$(curl http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/restui/user/account -H "X-CSRF-TOKEN: $X_CSRF_TOKEN" -H "Cookie: JSESSIONID=$JSESSIONID_H" | grep -oP '(?:accessKey\"\s\:\s\")\K(.*?)(?=\"\,)')
+		CONTROLLER_KEY=$(curl -s http://$CONTROLLER_HOST:$CONTROLLER_PORT/controller/restui/user/account -H "X-CSRF-TOKEN: $X_CSRF_TOKEN" -H "Cookie: JSESSIONID=$JSESSIONID_H" | grep -oP '(?:accessKey\"\s\:\s\")\K(.*?)(?=\"\,)')
 		rm cookie.appd
-		if [ -z $DB_AGENT_NAME ]; then
-			DB_AGENT_NAME=$(hostname)
-		fi 
-		if [ -z $DB_ENABLE_CONTAINERIDASHOSTID ]; then
-			DB_ENABLE_CONTAINERIDASHOSTID="false"
-		fi
-		DA_PROPERTIES="-Dappdynamics.controller.hostName=${CONTROLLER_HOST}"
-		DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.controller.port=${CONTROLLER_PORT}"
-		#DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.agent.accountName=${ACCOUNT_NAME}"
-		DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.agent.accountAccessKey=${CONTROLLER_KEY}"
-		#DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.controller.ssl.enabled=${CONTROLLER_SSL_ENABLED}"
-		DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.docker.container.containerIdAsHostId.enabled=${DB_ENABLE_CONTAINERIDASHOSTID}"
-		DA_PROPERTIES="$DA_PROPERTIES -Ddbagent.name=${DB_AGENT_NAME}"
-
-		DA_FILE=$DATABASE_AGENT_HOME/start-dbagent
-		if [ -f "$DA_FILE" ]; then
-			echo "Starting Database Agent"
-			# Start Database Agent
-			#echo java ${DA_PROPERTIES} -jar ${DA_FILE}
-			#java ${DA_PROPERTIES} -jar ${DA_FILE}
-			echo ${DA_FILE} ${DA_PROPERTIES}
-			chmod +x ${DA_FILE}
-			$DA_FILE ${DA_PROPERTIES}
-		else
-			echo "Database Agent File not found here - $DA_FILE"
-		fi
 	else
 		echo "Couldn't connect DA to controller to obtain controller key"
 	fi
 fi
 
+if [ ! -z $CONTROLLER_KEY ]; then
+	if [ -z $DB_AGENT_NAME ]; then
+		DB_AGENT_NAME=$(hostname)
+	fi 
+	if [ -z $DB_ENABLE_CONTAINERIDASHOSTID ]; then
+		DB_ENABLE_CONTAINERIDASHOSTID="false"
+	fi
+	DA_PROPERTIES="-Dappdynamics.controller.hostName=${CONTROLLER_HOST}"
+	DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.controller.port=${CONTROLLER_PORT}"
+	#DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.agent.accountName=${ACCOUNT_NAME}"
+	DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.agent.accountAccessKey=${CONTROLLER_KEY}"
+	#DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.controller.ssl.enabled=${CONTROLLER_SSL_ENABLED}"
+	DA_PROPERTIES="$DA_PROPERTIES -Dappdynamics.docker.container.containerIdAsHostId.enabled=${DB_ENABLE_CONTAINERIDASHOSTID}"
+	DA_PROPERTIES="$DA_PROPERTIES -Ddbagent.name=${DB_AGENT_NAME}"
+
+	DA_FILE=$DATABASE_AGENT_HOME/start-dbagent
+	if [ -f "$DA_FILE" ]; then
+		echo "Starting Database Agent"
+		# Start Database Agent
+		#echo java ${DA_PROPERTIES} -jar ${DA_FILE}
+		#java ${DA_PROPERTIES} -jar ${DA_FILE}
+		echo ${DA_FILE} ${DA_PROPERTIES}
+		chmod +x ${DA_FILE}
+		$DA_FILE ${DA_PROPERTIES}
+	else
+		echo "Database Agent File not found here - $DA_FILE"
+	fi
+fi 
