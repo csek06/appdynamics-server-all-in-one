b3da2049d90131e97d767d70c33d06ce82892161
Chris Sechler <sechlerc@mail.gvsu.edu>
2019-11-27 16:17:12 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -36,7 +36,7 @@ if [ ! "$COMPOSED" = "true" ]; then
 	if [ "$RHEL_OR_CENTOS" = "true" ]; then
 		echo "updating and installing packages"
 		sudo yum update -y
-		sudo yum install curl epel-release ibaio ncurses numactl tar tzdata unzip -y
+		sudo yum install curl epel-release libaio ncurses numactl tar tzdata unzip -y
 		sudo yum install python-pip -y
 		echo "updating firewall ports"
 		sudo firewall-cmd --zone=public --add-port=9191/tcp --permanent
